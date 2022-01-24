0c85d06d1a56eb9be3066587d5e1a5e5cc0a97c5
Zeyu-Li <zeyu.li23@gmail.com>
2022-01-23 21:39:31 -0600

    main + game page made

---
+++ pages/game/[id].js
@@ -0,0 +1,66 @@
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import styles from "../../styles/Home.module.css";
+
+export default function Game() {
+  const { id } = useRouter().query;
+  const [turn, changeTurn] = useState(true);
+  const [total, setTotal] = useState(0);
+  const nums = [1, 2, 3];
+
+  const addValue = (number) => {
+    if (!turn) return;
+    changeTurn(!turn);
+    setTotal(total + number);
+
+    // send to server + auth token
+    console.log(id);
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Don't say 10</title>
+        <meta name="description" content="Don't say 10 game" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Don't Say 10</h1>
+        <p className={styles.description}>
+          {turn ? "Your Turn" : "Waiting for opponent"}
+        </p>
+        <p className={styles.currentValue}>{total}</p>
+
+        <div className={styles.grid}>
+          {nums.map((num) => {
+            return (
+              <a
+                className={`${styles.card} ${
+                  turn ? "" : styles.selectableDisabled
+                }`}
+                onClick={() => addValue(num)}
+              >
+                <h2 style={{ textAlign: "center" }}>+{num}</h2>
+                <p style={{ textAlign: "center" }}>Adds {num} for your turn</p>
+              </a>
+            );
+          })}
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://andrewli.site"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Made by Andrew
+        </a>
+        <a href="/">Quit</a>
+      </footer>
+    </div>
+  );
+}
